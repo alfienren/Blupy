90417f754e513c675b1cf0ba947ec386cb1a4696
aschleg <aaron.schlegel@optimedia-us.com>
2015-01-08 14:24:55 -0800

    making more progress

--- arrange.py
+++ arrange.py
@@ -3,7 +3,7 @@
 
 ##### Load necessary packages
 
-# In[120]:
+# In[1]:
 
 import pandas as pd
 import numpy as np
@@ -13,30 +13,61 @@ import re
 
 ##### Open the working Excel sheet
 
-# In[121]:
+# In[2]:
 
 wb = Workbook("C:/Users/aarschle1/Google Drive/Optimedia/T-Mobile/Projects/Weekly_Reporting/Opti_DFA_Weekly_Reporting.xlsm")
 
 
 ##### A VBA subroutinue will create and add the required data to the sheets "SA_Temp" and "CFV_Temp". Create pandas DataFrames from this data.
 
-# In[122]:
+# In[91]:
 
-sa = Range("SA_Temp", "A2").table.value
-cfv = Range("CFV_Temp", "A2").table.value
+sa = Range("SA_Temp", "A1").table.value
+cfv = Range("CFV_Temp", "A1").table.value
 
 
 ##### Set the column names of the DataFrame
 
-# In[123]:
+# In[92]:
 
-sa = pd.DataFrame(sa, columns = Range("SA_Temp", "A1").horizontal.value)
-cfv = pd.DataFrame(cfv, columns = Range("CFV_Temp", "A1").horizontal.value)
+sa = pd.DataFrame(Range("SA_Temp", "A1").table.value, columns = Range("SA_Temp", "A1").horizontal.value)
+cfv = pd.DataFrame(Range("CFV_Temp", "A1").table.value, columns = Range("CFV_Temp", "A1").horizontal.value)
+
+sa = sa.drop(0)
+cfv = cfv.drop(0)
+
+
+##### Transform CFV Data
+
+# In[93]:
+
+cfv['Orders'] = 1
+
+
+# In[199]:
+
+cfv['Plans'] = cfv['Plan (string)'].str.count(',') + 1
+cfv['Devices'] = cfv['Device (string)'].str.count(',') + 1
+cfv['Services'] = cfv['Service (string)'].str.count(',') + 1
+cfv['Add-a-Line'] = cfv['Service (string)'].str.count('ADD')
+cfv['Accessories'] = cfv['Accessory (string)'].str.count(',') + 1
+
+cfv['Plans'] = cfv['Plans'].fillna(0)
+cfv['Devices'] = cfv['Devices'].fillna(0)
+cfv['Services'] = cfv['Services'].fillna(0)
+cfv['Add-a-Line'] = cfv['Add-a-Line'].fillna(0)
+cfv['Accessories'] = cfv['Accessories'].fillna(0)
+
+postpaid = np.where(cfv['Plans'] == cfv['Devices'], cfv['Plans'], pd.concat([cfv['Plans'], cfv['Devices']], axis=1).min(axis=1))
+prepaid = np.where((cfv['Plans'] == 0) & (cfv['Devices'] != 0), 0, cfv['Devices'])
+
+cfv['Postpaid Plans'] = postpaid
+cfv['Prepaid Plans'] = prepaid
 
 
 ##### Append the CFV data to the SA data and fill N/A values with 0.
 
-# In[124]:
+# In[278]:
 
 appended = sa.append(cfv)
 appended = appended.fillna(0)
@@ -44,7 +75,7 @@ appended = appended.fillna(0)
 
 ##### With the appended DataFrame, group the data, i.e. compress it, by each column below.
 
-# In[125]:
+# In[279]:
 
 appended = appended.groupby(['Campaign', 'Date', 'Site (DFA)', 'Creative', 'Click-through URL', 'Ad', 'Creative Groups 1',
                              'Creative Groups 2', 'Creative ID', 'Creative Type', 'Creative Field 1', 'Placement',   
@@ -52,16 +83,9 @@ appended = appended.groupby(['Campaign', 'Date', 'Site (DFA)', 'Creative', 'Clic
                              'Plan (string)', 'Service (string)']).aggregate(np.sum)
 
 
-##### Again, fill in N/A values with 0
-
-# In[126]:
-
-appended = appended.fillna(0)
-
-
 ##### Copy the new DataFrame into the Excel sheet on the working tab. Create a new DataFrame off of this data
 
-# In[127]:
+# In[280]:
 
 Range('working', 'A1').value = appended
 appended = pd.DataFrame(Range('working', 'A2').table.value, columns=Range('working', 'A1').horizontal.value)
@@ -70,7 +94,7 @@ Range('working', 'A1').value = appended
 
 ##### Using the list of actions in the 'Action Reference' tab of the Excel sheet, set lists for each action category.
 
-# In[128]:
+# In[281]:
 
 a_actions = Range('Action_Reference', 'A2').vertical.value
 b_actions = Range('Action_Reference', 'B2').vertical.value
@@ -83,7 +107,7 @@ col_head = Range('working', 'A1').horizontal.value
 
 ##### Set the actions to lists and search the DataFrame columns for each one, summing each value when found.
 
-# In[129]:
+# In[282]:
 
 a_actions = list(set(a_actions).intersection(col_head))
 b_actions = list(set(b_actions).intersection(col_head))
@@ -92,7 +116,7 @@ d_actions = list(set(d_actions).intersection(col_head))
 e_actions = list(set(e_actions).intersection(col_head))
 
 
-# In[130]:
+# In[283]:
 
 view_through = []
 i = iter(view_through)
@@ -111,7 +135,7 @@ for item in col_head:
         j.next()
 
 
-# In[131]:
+# In[284]:
 
 view_based = list(set(view_through).intersection(col_head))
 click_based = list(set(click_through).intersection(col_head))
@@ -119,7 +143,7 @@ click_based = list(set(click_through).intersection(col_head))
 
 ##### Add columns to the DataFrame for each action category
 
-# In[132]:
+# In[285]:
 
 appended['A Actions'] = appended[a_actions].sum(axis=1)
 appended['B Actions'] = appended[b_actions].sum(axis=1)
@@ -133,7 +157,7 @@ appended['View Based'] = appended[view_based].sum(axis=1)
 
 ##### Store Locator
 
-# In[133]:
+# In[286]:
 
 store_locator = []
 k = iter(store_locator)
@@ -144,7 +168,7 @@ for item in col_head:
         k.next()
 
 
-# In[134]:
+# In[287]:
 
 SLV_conversions = list(set(store_locator).intersection(col_head))
 appended['Store Locator Visits'] = appended[store_locator].sum(axis=1)
@@ -152,7 +176,7 @@ appended['Store Locator Visits'] = appended[store_locator].sum(axis=1)
 
 ##### Traffic Action Totals
 
-# In[135]:
+# In[288]:
 
 appended['Awareness Actions'] = appended['A Actions'] + appended['B Actions']
 appended['Consideration Actions'] = appended['C Actions'] + appended['D Actions']
@@ -161,7 +185,7 @@ appended['Traffic Actions'] = appended['Awareness Actions'] + appended['Consider
 
 ##### Message Categories
 
-# In[155]:
+# In[289]:
 
 appended['Creative Field 1'] = appended['Creative Field 1'].str.replace('Creative Type: ', '')
 
@@ -179,7 +203,7 @@ appended['Message Offer'].fillna(appended['Creative Groups 2'], inplace=True)
 
 ##### Strip the embedded URL encoding used by BlueKai to get the actual URL.
 
-# In[102]:
+# In[272]:
 
 appended['Click-through URL'] = appended['Click-through URL'].str.replace('http://analytics.bluekai.com/site/', '')
 appended['Click-through URL'] = appended['Click-through URL'].str.replace('15991\?phint', '')
