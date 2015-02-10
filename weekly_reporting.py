5202e2e614faeef97aed7c7b42de86a1cc4918df
aschleg <aaron.schlegel@optimedia-us.com>
2015-02-10 13:12:38 -0800

    edit to prepaid logic

--- weekly_reporting.py
+++ weekly_reporting.py
@@ -58,7 +58,7 @@ def dfa_reporting():
     cfv['Devices'] = cfv['Device (string)'].str.count(',') + 1 # Count number of devices in the Plans column
     cfv['Services'] = cfv['Service (string)'].str.count(',') + 1 # Count number of services in the Plans column
     cfv['Add-a-Line'] = cfv['Service (string)'].str.count('ADD') # Count number of Add-a-Lines in the Plans column
-    cfv['Accessories'] = cfv['Accessory (string)'].str.count(',') + 1 # Count number of Accessories in the Plans column
+    cfv['Accessories'] = cfv['Accessory (string)'].str.count(',') + 1 # Count number of Accessories in the Accessories column
     cfv['Activations'] = cfv['Plans'] + cfv['Add-a-Line'] #Activations are defined as the sum of Plans and Add-a-Line
 
     # Postpaid plans are defined as a Plan + Device. By row, if number of plans is equal to number of devices, Postpaid
@@ -68,7 +68,9 @@ def dfa_reporting():
 
     # Prepaid plans are defined as the number of Devices with no service and plan. If number of plans and services are
     # 0, count of devices is prepaid. If service and plan are not equal, prepaid plans = 0.
-    cfv['Prepaid Plans'] = np.where((cfv['Plans'] == 0) & (cfv['Services'] == 0), cfv['Devices'], 0)
+    cfv['Prepaid Plans'] = np.where((cfv['Plans'] == 0) & (cfv['Services'] == 0), cfv['Devices'],
+                                    np.where(cfv['Devices'] > (cfv['Plans'] & cfv['Services']),
+                                             cfv['Devices'] - pd.concat([cfv['Plans'], cfv['Services']], axis=1).max(axis=1), 0))
 
     # The DDR campaign counts view-through order credit at 50%. If the campaign name contains 'DDR' and the Floodlight
     # Attribution Type is View-through, the order is multiplied by 0.5.
