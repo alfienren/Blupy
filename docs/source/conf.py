9cce134a2e2d091b169d82f42d50e263408ac25c
aschleg <aaron.schlegel@optimedia-us.com>
2015-02-10 15:16:13 -0800

    edits

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -71,7 +71,7 @@ copyright = u'2015, Aaron Schlegel'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['html', 'latex']
+exclude_patterns = []
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
@@ -82,7 +82,7 @@ exclude_patterns = ['html', 'latex']
 
 # If true, the current module name will be prepended to all description
 # unit titles (such as .. function::).
-add_module_names = True
+#add_module_names = True
 
 # If true, sectionauthor and moduleauthor directives will be shown in the
 # output. They are ignored by default.
