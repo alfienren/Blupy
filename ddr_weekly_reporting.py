2f96c862eae1c9291165c33b114e7784aee49c0a
aschleg <aaron.schlegel@optimedia-us.com>
2015-02-19 09:59:52 -0800

    add top 15 plans to summary

--- ddr_weekly_reporting.py
+++ ddr_weekly_reporting.py
@@ -19,21 +19,37 @@ def ddr_top_15_devices():
     ddr = pd.DataFrame(pd.read_csv(sheet[:sheet.rindex('\\')] + '\\_\\devices.csv'))
 
     cfv['Device IDs'] = cfv['Device (string)'].str.split(',')
+
+    cfv['Plan Names'] = cfv['Plan (string)'].str.split(',')
+
     ddr_devices = pd.Series(list(np.where((cfv['Campaign'].str.contains('DDR') == True) |
                                           (cfv['Campaign'].str.contains('Q1_Brand Remessaging') == True),
                                           cfv['Device IDs'], np.NaN)))
 
+    ddr_plans = pd.Series(list(np.where((cfv['Campaign'].str.contains('DDR') == True) |
+                                        (cfv['Campaign'].str.contains('Q1_Brand Remessaging') == True),
+                                        cfv['Plan Names'], np.NaN)))
+
     ddr_devices.dropna(inplace = True)
+    ddr_plans.dropna(inplace = True)
+
     ddr_devices = list(itertools.chain(*ddr_devices))
+    ddr_plans = list(itertools.chain(*ddr_plans))
 
     while '' in ddr_devices: ddr_devices.remove('')
+    while '' in ddr_plans: ddr_plans.remove('')
 
     device_counts = pd.DataFrame(pd.value_counts(pd.Series(ddr_devices).values, sort = True)[0:15])
     device_counts['Device Name'] = 1
     device_counts['Subcategory'] = 1
+    device_counts['Subcategory - Device'] = 1
+
+    plan_counts = pd.DataFrame(pd.value_counts(pd.Series(ddr_plans).values, sort = True)[0:15])
 
     Range('DDR', 'A1').value = ddr
     Range('Summary', 'B1').value = device_counts
+
+    Range('Summary', 'I1').value = plan_counts
 
     Sheet('Summary').activate()
 
@@ -41,6 +57,11 @@ def ddr_top_15_devices():
     for cell in Range('Summary', 'A2:' + 'A' + str(len(device_counts) + 1)):
         i = i + 1
         cell.value = i
+
+    j = 0
+    for cell in Range('Summary', 'H2:' + 'H' + str(len(device_counts) + 1)):
+        j = j + 1
+        cell.value = j
 
     for cell in Range('Summary', 'D2').vertical:
         id = cell.offset(0, -2).get_address(False, False, False)
