0674bbf86867cb9cd9af80e483d79fc820f79ea6
aschleg <aaron@aaronschlegel.com>
2015-05-20 10:27:37 -0700

    rename __main__ to work with Excel

--- dfaReporting/weekly_reporting.py
+++ dfaReporting/weekly_reporting.py
@@ -1,13 +1,13 @@
 from weekly import data_load, ExcelImport, cfv, categorization, action_tags, clickthroughs, floodlight, \
     dfa_data_output, f_tags
 from xlwings import Workbook, Range, Sheet
-import os
 import pandas as pd
+import os
 
 def weekly_reporting():
 
     wb = Workbook.caller()
-
+    wb.save()
     sa = data_load.sa_data()
 
     floodlights = data_load.cfv_data()
@@ -36,10 +36,12 @@ def weekly_reporting():
         appended_data.fillna(0, inplace=True)
         Sheet('data').clear()
         ExcelImport.chunk_df(appended_data, 'data', 'A1', 5000)
-
+        
+'''
 if __name__ == '__main__':
     # To run from Python, not needed when called from Excel.
     # Expects the Excel file next to this source file, adjust accordingly.
-    path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'dfa_test.xlsm'))
+    path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'DFA_Weekly_Reporting.xlsm'))
     Workbook.set_mock_caller(path)
     weekly_reporting()
+'''
