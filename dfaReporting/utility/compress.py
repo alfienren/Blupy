e3be63f32d42e0cae0a4e607ee991004e9b02928
aschleg <aaron@aaronschlegel.com>
2015-05-28 16:05:26 -0700

    add py file for compressing data for internal team use

--- dfaReporting/utility/compress.py
+++ dfaReporting/utility/compress.py
@@ -2,28 +2,33 @@ import pandas as pd
 from xlwings import Workbook, Range, Sheet
 from weekly import data_output
 import os
+from win32com.client import Dispatch
 
 def compress_data():
 
+    Workbook.caller()
     path = Range('Tools', 'ZZ1').value
-    workbook_path = os.path.normpath(path)
-    output_path = os.path.normpath(path[:path.rindex('\\')])
-
-    data = pd.read_excel(workbook_path, 'data', index_col = None)
+    output_path = path[:path.rindex('\\')]
+    joinpath = os.path.join(output_path, 'compressed_data.xlsm')
+    data = pd.read_excel(path, 'data', index_col = None)
 
     columns = list(data.columns)
-    columns = columns[columns.index('Post-Impression Activity') + 1:]
+    columns = columns[:columns.index('Post-Impression Activity') + 1]
 
+    Range('Tools', 'ZZ2').value = joinpath
+    xl = Dispatch('Excel.Application')
+    xlwb = xl.Workbooks.Add()
+    xlws = xlwb.Worksheets('Sheet1')
+    xlws.Name = 'data'
+    xlOpenXMLWorkbookMacroEnabled = 52
+    xlwb.SaveAs(joinpath, FileFormat = xlOpenXMLWorkbookMacroEnabled)
+    xlwb.Close()
+
+    wb = Workbook(joinpath)
+    wb.set_current()
     data = data[columns]
-
-    wb = Workbook()
-
-    Sheet(1).name = 'data'
-
     data_output.chunk_df(data, 'data', 'A1', 2000)
 
-    wb.save(output_path + 'compressed_data.xlsx')
-    wb.close()
 
 
 
