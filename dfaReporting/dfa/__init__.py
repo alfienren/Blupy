6612ed6765be4795c112f0d9105ff9be3d805ebc
aschleg <aaron@aaronschlegel.com>
2015-10-06 16:04:33 -0700

    add init file

--- dfaReporting/dfa/__init__.py
+++ dfaReporting/dfa/__init__.py
@@ -1,2 +1,2 @@
-__all__ = ['action_tags', 'categorization', 'cfv_clean', 'clickthroughs', 'data_output', 'f_tags', 'floodlight_transform',
+__all__ = ['action_tags', 'categorization', 'cfv_report', 'clickthroughs', 'data_output', 'f_tags', 'floodlights',
        'load_dfa_reports', 'messages']
