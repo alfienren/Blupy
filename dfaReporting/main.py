d32a82f1184508328dc46ab49206d87e6df2b552
aschleg <aaron@aaronschlegel.com>
2015-10-07 11:52:30 -0700

    add new functions

--- dfaReporting/main.py
+++ dfaReporting/main.py
@@ -1,11 +1,12 @@
+import re
+
+from xlwings import Workbook, Range, Sheet
+import pandas as pd
+
 from dfa import *
 from utility import *
 from datafeeds import *
 
-from xlwings import Workbook, Range, Sheet
-import pandas as pd
-import numpy as np
-import re
 
 def chunk_df(df, sheet, startcell, chunk_size):
 
@@ -22,50 +23,6 @@ def chunk_df(df, sheet, startcell, chunk_size):
                       range(0, len(df), chunk_size)):
             Range(sheet, row + str(col), index=False, header=False).value = chunk
             col += chunk_size
-
-def dfa_additional_columns(data):
-
-    # The DFA field DBM Cost is more accurate for placements using dynamic bidding. If a placement is not using
-    # dynamic bidding, DBM Cost = 0. Therefore, if DBM cost does not equal 0, replace the row's media cost with
-    # DBM cost. If DBM Cost = 0, Media Cost stays the same.
-    data['Media Cost'] = np.where(data['DBM Cost USD'] != 0, data['DBM Cost USD'], data['Media Cost'])
-
-    # Adjust spend to Net to Client
-    data['NTC Media Cost'] = 0
-
-    # DBM Cost column is then removed as it is no longer needed.
-    data.drop('DBM Cost USD', 1, inplace=True)
-
-    # Add columns for Video Completions and Views, primarily for compatibility between campaigns that run video and
-    # don't run video. Those that don't can keep the columns set to zero, but those that have video can then be
-    # adjusted with passback data.
-    data['Video Completions'] = 0
-    data['Video Views'] = 0
-
-    return data
-
-def order_columns():
-
-    dimensions = ['Week', 'Date', 'Month', 'Quarter', 'Campaign', 'Language', 'Site (DCM)', 'Site', 'Click-through URL',
-                  'F Tag', 'Category', 'Message Bucket', 'Message Category', 'Creative Bucket', 'Creative Theme',
-                  'Creative Type', 'Creative Groups 1', 'Creative ID', 'Creative', 'Ad', 'Creative Groups 2',
-                  'Creative Field 1', 'Placement', 'Placement ID', 'Placement Cost Structure']
-
-    cfv_floodlight_columns = ['OrderNumber (string)',  'Activity','Floodlight Attribution Type',
-                              'Plan (string)', 'Device (string)', 'Service (string)', 'Accessory (string)']
-
-    metrics = ['Media Cost', 'NTC Media Cost', 'Impressions', 'Clicks', 'Orders', 'Plans', 'Add-a-Line',
-               'Activations', 'Devices', 'Services', 'Accessories',
-               'Postpaid Plans', 'Prepaid Plans', 'eGAs', 'Store Locator Visits', 'A Actions', 'B Actions', 'C Actions',
-               'D Actions', 'E Actions', 'F Actions', 'Awareness Actions', 'Consideration Actions',
-               'Traffic Actions', 'Post-Click Activity', 'Post-Impression Activity', 'Video Views',
-               'Video Completions', 'Prepaid GAs', 'Postpaid GAs', 'Prepaid SIMs', 'Postpaid SIMs',
-               'Prepaid Mobile Internet', 'Postpaid Mobile Internet', 'Prepaid Phone', 'Postpaid Phone',
-               'DDR New Devices', 'DDR Add-a-Line']
-
-    new_columns = dimensions + metrics + cfv_floodlight_columns
-
-    return list(new_columns)
 
 def weekly_reporting():
 
@@ -84,6 +41,10 @@ def weekly_reporting():
     sheet = Range('Lookup', 'AA1').value
     sa = pd.read_excel(sheet, 'SA_Temp', index_col=None)
 
+    sa_creative = sa[['Placement', 'Creative Field 1']]
+
+    cfv = pd.merge(cfv, sa_creative, how = 'left', on = 'Placement')
+
     cfv = cfv_report.run_cfv_macro(cfv)
 
     data = sa.append(cfv)
@@ -95,10 +56,12 @@ def weekly_reporting():
 
     data = floodlights.f_tags(data)
 
+    data = report_columns.additional(data)
+
     sa_columns = list(sa.columns)
     tag_columns = sa_columns[sa_columns.index('DBM Cost USD') + 1:]
 
-    columns = order_columns() + tag_columns
+    columns = report_columns.order() + tag_columns
 
     data = data[columns]
     data.fillna(0, inplace=True)
@@ -115,7 +78,9 @@ def weekly_reporting():
         appended_data = appended_data[columns]
         appended_data.fillna(0, inplace=True)
         Sheet('data').clear()
-        chunk_df(appended_data, 'data', 'A1', 3000)
+        chunk_df(appended_data, 'data', 'A1', 5000)
+
+    qa.placement_qa(data)
 
 def data_compression():
 
