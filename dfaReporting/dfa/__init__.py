6ffc36983240b133406e6d140d490cfedaf6b74a
aschleg <aaron@aaronschlegel.com>
2015-10-07 09:22:18 -0700

    remove old files

--- dfaReporting/dfa/__init__.py
+++ dfaReporting/dfa/__init__.py
@@ -1,2 +1,1 @@
-__all__ = ['action_tags', 'categorization', 'cfv_report', 'clickthroughs', 'data_output', 'f_tags', 'floodlights',
-       'load_dfa_reports', 'messages']
+__all__ = ['categorization', 'cfv_report', 'clickthroughs', 'floodlights']
