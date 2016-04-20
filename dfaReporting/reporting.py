04406da49f4eb04bb4f56b6284875cd9b1f84edd
aschleg <aaron@aaronschlegel.com>
2016-04-20 16:38:51 -0700

    updates

--- dfaReporting/reporting.py
+++ dfaReporting/reporting.py
@@ -2,13 +2,9 @@ from xlwings import Workbook
 from weekly_reporting import *
 from reporting_data import *
 from paths import *
-import campaign_reports
-from campaign_reports import costfeed
-from campaign_reports.ddr.pacing import *
-from campaign_reports.ddr.tableau import *
+from campaign_reports.ddr.dashboard import *
 
 import pandas as pd
-import numpy as np
 
 
 def generate_weekly_reporting():
@@ -20,9 +16,9 @@ def generate_weekly_reporting():
     # Workbook needs to be saved in order to load the data into pandas properly
     # Load the Site Activity and Custom Floodlight Variable data into pandas as DataFrames
 
-    sa, sa_creative = read_site_activity_report(adv='tmo')
+    sa, sa_creative = read_site_activity_report(wb.fullname, adv='tmo')
 
-    cfv = pd.merge(read_cfv_report(), sa_creative, how = 'left', on = 'Placement')
+    cfv = pd.merge(read_cfv_report(wb.fullname), sa_creative, how = 'left', on = 'Placement')
 
     cfv = custom_variables.custom_variable_columns(cfv)
     cfv = custom_variables.ddr_custom_variables(cfv)
@@ -54,8 +50,8 @@ def generate_metro_reporting():
     wb = Workbook.caller()
     wb.save()
 
-    sa = read_site_activity_report(adv='metro')
-    cfv = read_cfv_report()
+    sa = read_site_activity_report(wb.fullname, adv='metro')
+    cfv = read_cfv_report(wb.fullname)
 
     data = sa.append(cfv)
     data = floodlights.a_e_traffic(data, adv='metro')
@@ -83,40 +79,6 @@ def generate_wfm_reporting():
 
 def generate_dr_reporting():
     dr.dr_weekly_reporting()
-
-
-def output_flat_rate_report():
-    Workbook.caller()
-
-    campaign_reports.cost_corrections.flat_rate_corrections()
-
-
-def pacing_report():
-    campaign_reports.campaign_pacing.site_pacing_report()
-
-
-def build_traffic_master():
-    wb = Workbook.caller()
-    path = wb.fullname
-
-    master_sheet = campaign_reports.traffic_master.merge_traffic_sheets()
-
-    output_path = path[:path.rindex('\\')] + '/' + 'Trafficking_Master.xlsx'
-
-    traffic_sheet = Workbook()
-
-    chunk_df(master_sheet, 0, 'A1')
-
-    traffic_sheet.save(output_path)
-
-    traffic_sheet.close()
-    wb.set_current()
-
-
-def tmo_costfeed():
-    Workbook.caller()
-
-    costfeed.cost_feed()
 
 
 def dr_generate_dashboard_data():
@@ -154,44 +116,3 @@ def dr_generate_dashboard_data():
 
     wb2.save(save_path + '\\' + 'DR_Raw_Data.csv')
     wb2.close()
-
-
-def output_forecasts(pacing_data):
-    pacing_data['Week'] = pacing_data['Date'].apply(lambda x: categorization.mondays(x))
-
-    pacing_data = pd.pivot_table(pacing_data, index= ['Site', 'Tactic', 'Metric'],
-                          columns= ['Week'], values= 'value', aggfunc= np.sum).reset_index()
-
-    wb = Workbook(paths.dr_pacing_path())
-
-    Sheet('forecast_data').clear_contents()
-    Range('forecast_data', 'A1', index= False).value = pacing_data
-
-    wb.save()
-    wb.close()
-
-
-def dr_pacing_data_for_forecasts():
-    wb = Workbook.caller()
-
-    dr_data = display.raw_pivot()
-
-    dr_forecasting = data_transform.transform_dr_forecasts(dr_data)
-    wb.set_current()
-
-    Sheet('raw_pacing_data').clear_contents()
-    Range('raw_pacing_data', 'A1', index=False).value = dr_forecasting
-
-    performance.publishers(dr_data)
-
-
-def dr_reshape_forecasts():
-    Workbook.caller()
-
-    r_data = forecast.generate_forecasts()
-
-    pacing_data = forecast.merge_pacing_and_forecasts(r_data)
-
-    tab = display.tableau_pacing(pacing_data)
-
-    forecast.output_forecasts(tab)
