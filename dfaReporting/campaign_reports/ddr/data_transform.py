803533800d01b5868c132d899c1cce47b866e06e
aschleg <aaron@aaronschlegel.com>
2016-04-21 13:59:29 -0700

    move functions to forecast.py

--- dfaReporting/campaign_reports/ddr/data_transform.py
+++ dfaReporting/campaign_reports/ddr/data_transform.py
@@ -1,8 +1,9 @@
-import main
-import pandas as pd
-import numpy as np
 import arrow
-from xlwings import Application, Workbook, Range
+import numpy as np
+import pandas as pd
+from xlwings import Range, Workbook, Application
+import performance
+import paths
 
 
 def drop_columns(dr):
@@ -23,62 +24,7 @@ def dr_sites():
     return sites
 
 
-def dr_placement_types():
-    cd = '|'.join(list(['C/D', 'C Pages', 'D Pages']))
-    t2t = '|'.join(list(['Tablet to Tablet']))
-    fbx = '|'.join(list(['FBX ']))
-    search = '|'.join(list(['Search']))
-    pros = '|'.join(list(['Prospecting']))
-    aal = '|'.join(list(['Add']))
-    tap_att = '|'.join(list(['Tap-to-Call (AT&T)']))
-    tap_other = '|'.join(list(['Tap-to-Call (Other)']))
-    tap_verizon = '|'.join(list(['Tap-to-Call (Verizon)']))
-    tap_sprint = '|'.join(list(['Tap-to-Call (Sprint)']))
 
-    return (cd, t2t, fbx, search, pros, aal, tap_att, tap_other, tap_sprint, tap_verizon)
-
-
-def transform_dr_forecasts(dr):
-    dr = dr[dr['Campaign'] == 'DR']
-    ddr = drop_columns(dr)
-
-    cd, t2t, fbx, search, pros, aal, tap_att, tap_other, tap_sprint, tap_verizon = dr_placement_types()
-
-    pacing_dr = ddr.rename(columns={'Placement Messaging Type': 'Type', 'NTC Media Cost': 'Spend', 'Total GAs': 'GAs'})
-
-    pacing_dr['Type.Agg'] = np.where(
-        pacing_dr['Type'].str.contains(fbx) == True, 'FBX.Remessaging',
-        np.where(pacing_dr['Type'].str.contains(t2t) == True, 'Tablet-to-Tablet',
-                 np.where(pacing_dr['Type'].str.contains(cd) == True, 'CD.Remessaging',
-                          np.where(pacing_dr['Type'].str.contains(search) == True, 'Search.Remessaging',
-                                   np.where(pacing_dr['Type'].str.contains(pros) == True, 'Prospecting',
-                                            'Add-A-Line')))))
-
-    pacing_dr = pacing_dr[(pacing_dr['Date'] >= main.quarter_start()) &
-                          (pacing_dr['Site'].str.contains(dr_sites()) == True)]
-
-    pacing_dr = pacing_dr.groupby(['Site', 'Type.Agg', 'Type', 'Date'])
-
-    pacing_dr = pd.DataFrame(pacing_dr.sum()).reset_index()
-
-    pacing_dr['Site_Type'] = pacing_dr['Site'] + '.' + pacing_dr['Type.Agg']
-
-    pacing_pivoted = pd.pivot_table(pacing_dr, values=['Spend', 'GAs'], index=['Date'], columns=['Site_Type'],
-                               aggfunc=np.sum)
-    pacing_pivoted.fillna(0, inplace=True)
-
-    spend = pacing_pivoted['Spend']
-    gas = pacing_pivoted['GAs']
-
-    spend.columns = pd.Series(spend.columns).astype(str) + '.Spend'
-    gas.columns = pd.Series(gas.columns).astype(str) + '.GAs'
-
-    merged = pd.merge(spend, gas, how= 'left', right_index= True, left_index= True)
-    merged.reset_index(inplace=True)
-
-    merged['Weekday'] = merged['Date'].apply(lambda x: arrow.get(x).format('dddd'))
-
-    return merged
 
 
 def q4_pacing():
@@ -89,10 +35,10 @@ def q4_pacing():
     e = '2015-12-28'
     f = '2015-12-31'
 
-    date_rng = pd.date_range(main.quarter_start(), periods=92, freq='D')
+    date_rng = pd.date_range(performance.quarter_start_year(), periods=92, freq='D')
     date_df = pd.DataFrame(date_rng, columns=['Date'])
 
-    pacing_wb = Workbook(main.dr_pacing_path())
+    pacing_wb = Workbook(paths.dr_pacing_path())
     pacing_wb.set_current()
 
     Application(wkb=pacing_wb).xl_app.Run('Clean_Pacing_Data')
