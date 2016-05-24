8d2fdc2ab09cc1b4c5f47c7d4111d21fec90e8f2
aschleg <aaron@aaronschlegel.com>
2016-05-24 09:20:22 -0700

    update to latest

--- dfaReporting/reporting/ddr/performance/common.py
+++ dfaReporting/reporting/ddr/performance/common.py
@@ -6,27 +6,6 @@ def performance_sheet():
     return pub_performance_sheet
 
 
-def qquarter():
-    quarter = 'Q2'
-
-    return quarter
-
-
-def quarter_start_year(start='quarter_start'):
-    if start != 'quarter_start':
-        quarter = '4/18/2016'
-    else:
-        quarter = '4/1/2016'
-
-    return quarter
-
-
-def week_of(dr):
-    week = 'Spend Week of ' + str(dr['Week'].max().strftime('%m/%d/%Y').lstrip('0').replace(' 0', ' '))
-
-    return week
-
-
 def row():
     row_num = 13
 
