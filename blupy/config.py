993fabf711270095a668768f8d5e77816d43974c
aschleg <aaron@aaronschlegel.com>
2017-02-24 14:32:26 -0800

    add relative path for config file

--- blupy/config.py
+++ blupy/config.py
@@ -1,11 +1,13 @@
 import json
+import os
 
 
 class configFile(object):
 
     def __init__(self):
-        super(configFile, self).__init__()
-        self.config_file = 'config.json'
+        #super(configFile, self).__init__()
+        self.config_file = os.path.join(os.path.dirname(__file__), 'config.json')
+        self.configs = self.load_config()
 
     def load_config(self):
         with open(self.config_file) as configfile:
