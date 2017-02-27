872bcd15bdff0b49e430453ccb8ed9ff6a58fd92
aschleg <aaron@aaronschlegel.com>
2017-02-27 09:59:31 -0800

    update to include config variables

---
+++ blupy/dcm/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ['dcm_api', 'trafficking']
--- blupy/dcm/dcm_api.py
+++ blupy/dcm/dcm_api.py
@@ -7,11 +7,13 @@ from oauth2client.tools import run_flow, argparser
 from apiclient.discovery import build
 
 from xlwings import Workbook
+from config import configFile
 
 
-class DCM_API(object):
+class DCM_API(configFile):
 
     def __init__(self):
+        super(DCM_API, self).__init__()
         Workbook.caller()
         self.API_NAME = 'dfareporting'
         self.API_VERSION = 'v2.7'
@@ -25,13 +27,14 @@ class DCM_API(object):
         self.GENERATE_FLOODLIGHT_TAGS = 'Generate_Tags'
         self.UPDATE_FLOODLIGHTS = 'Update_Floodlights'
         self.PIGGYBACK_PIXELS = 'Implement_Pixels'
+        self.DELETE_PIXELS = 'Delete_Pixels'
         self.TRAFFIC_SHEET = 'Placement_Traffic'
         self.CAMPAIGN_SHEET = 'Campaigns'
         self.LIST_FLOODLIGHT_PIXELS = 'Get_Pixels'
         self.SITEMAP = 'Get_Sitemap'
 
     def authenticate_user(self):
-        storage_path = r'S:\SEA-Media\Analytics\T-Mobile\_\api_credentials\user_credentials\saved_user_creds_'
+        storage_path = self.configs['paths']['user_credentials']
         user_name = getpass.getuser()
 
         storage = Storage(storage_path + user_name + '.dat')
@@ -40,7 +43,7 @@ class DCM_API(object):
 
         if credentials is None or credentials.invalid:
             credentials = run_flow(
-                flow_from_clientsecrets(r'S:\SEA-Media\Analytics\T-Mobile\_\api_credentials\client_secret.json',
+                flow_from_clientsecrets(self.configs['paths']['client_secret'],
                                         scope=self.API_SCOPES),
                 storage, argparser.parse_args([]))
 
