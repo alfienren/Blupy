75b2aa269f57290c13634da6416ce03c340930d8
aschleg <aaron@aaronschlegel.com>
2017-03-31 16:21:39 -0700

    functions for automated report and references

--- blupy/analytics/data/streams.py
+++ blupy/analytics/data/streams.py
@@ -2,6 +2,7 @@ from dcm.reports import DCMReports
 from dcm.dcm_api import DCM_API
 from floodlights import Floodlights
 from file_io import DataMethods
+import numpy as np
 
 
 class DatoramaStreams(DCMReports):
@@ -14,13 +15,13 @@ class DatoramaStreams(DCMReports):
     def custom_floodlights(self):
         save_path = r'C:/Users/aarschle1/Desktop/cfv2.xlsx'
         repId = self.configs['report_ids']['datoramaCFV']
-        cfv = self.run_and_download_report(repId, save_path)
+        cfv = self.run_and_download_report(repId)
 
-        #cfv = pd.read_excel(save_path)
         cfv = Floodlights().custom_variables(cfv)
-        cfv = Floodlights().ddr_custom_variables(cfv)
+        del cfv['Device_reg']
 
         DataMethods().chunk_df(cfv, 'Sheet1', 'A1')
 
     def reference_table(self):
-        pass
+        repId = self.configs['report_ids']['datoramaReferenceTable']
+        ref = self.run_and_download_report(repId)
